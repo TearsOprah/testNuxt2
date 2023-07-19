a2df7aca7405ee35e5e88e39733edad8491c7f39
TearsOprah <qmeshokp@mail.ru>
2023-07-20 01:37:04 +0300

    feat: сортировка по id

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>Тестовое задание</h1>
+
+    <label for="sortSelect">Сортировать по:</label>
+    <select id="sortSelect" v-model="sortOrder" @change="fetchComments">
+      <option value="asc">По возрастанию</option>
+      <option value="desc">По убыванию</option>
+    </select>
+
     <ul>
       <li v-for="comment in comments" :key="comment.id" @click="goToComment(comment.id)">
         <p>{{ comment.name }}</p>
@@ -15,6 +22,7 @@ export default {
   data() {
     return {
       comments: [],
+      sortOrder: 'asc',
     }
   },
   async asyncData({ $axios }) {
@@ -29,6 +37,15 @@ export default {
   methods: {
     goToComment(commentId) {
       this.$router.push(`/comments/${commentId}`);
+    },
+    async fetchComments() {
+      try {
+        const response = await this.$axios.$get(`comments?_sort=id&_order=${this.sortOrder}`);
+        this.comments = response;
+      } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+        this.comments = [];
+      }
     },
   }
 }
