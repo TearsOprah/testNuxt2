617139d060e1c12160d4a19bd2e87ae75a1b330e
TearsOprah <qmeshokp@mail.ru>
2023-07-20 02:03:40 +0300

    feat: пагинация + теперь происходит запрос не всех данных разом, а по 10 штук

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <h1>Тестовое задание</h1>
 
     <label for="sortSelect">Сортировать по:</label>
-    <select id="sortSelect" v-model="sortOrder" @change="fetchComments">
+    <select id="sortSelect" v-model="sortOrder" @change="onSortOrderChange">
       <option value="asc">По возрастанию</option>
       <option value="desc">По убыванию</option>
     </select>
@@ -13,6 +13,10 @@
         <p>{{ comment.name }}</p>
       </li>
     </ul>
+
+    <button @click="prevPage" :disabled="currentPage === 1">Предыдущая</button>
+    <button @click="nextPage" :disabled="comments.length < 10">Следующая</button>
+
   </div>
 </template>
 
@@ -23,7 +27,9 @@ export default {
     return {
       comments: [],
       sortOrder: 'asc',
-    }
+      currentPage: 1,
+      perPage: 10,
+    };
   },
 
   async created() {
@@ -31,17 +37,36 @@ export default {
   },
 
   methods: {
+
     goToComment(commentId) {
       this.$router.push(`/comments/${commentId}`);
     },
+
     async fetchComments() {
       try {
-        this.comments = await this.$axios.$get(`comments?_sort=id&_order=${this.sortOrder}`);
+        this.comments = await this.$axios.$get(`comments?_sort=id&_order=${this.sortOrder}&_page=${this.currentPage}&_limit=${this.perPage}`);
       } catch (error) {
         console.error('Ошибка при получении данных:', error);
         this.comments = [];
       }
     },
-  }
-}
+
+    async prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+        await this.fetchComments();
+      }
+    },
+
+    async nextPage() {
+      this.currentPage++;
+      await this.fetchComments();
+    },
+
+    onSortOrderChange() {
+      this.currentPage = 1; // сбросить текущую страницу на 1 при изменении фильтра
+      this.fetchComments();
+    },
+  },
+};
 </script>
