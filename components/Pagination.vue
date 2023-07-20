38c4cb3134bc82d1549ae4f59d44b42273b5430f
TearsOprah <qmeshokp@mail.ru>
2023-07-20 09:55:13 +0300

    refactor: пагинация вынесена в отдельный компонент

---
+++ components/Pagination.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button class="comments__button" @click="prevPage" :disabled="currentPage === 1">Предыдущая</button>
+    <button class="comments__button" @click="nextPage" :disabled="comments.length < 10">Следующая</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+    hasMoreComments: {
+      type: Boolean,
+      required: true,
+    },
+    comments: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    prevPage() {
+      this.$emit('prev-page');
+    },
+    nextPage() {
+      this.$emit('next-page');
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,36 @@
 <template>
-  <div>
-    <h1>Тестовое задание</h1>
+  <div class="comments">
+    <!-- Заголовок страницы -->
+    <h1 class="comments__title">Тестовое задание</h1>
 
+    <!-- Форма выбора сортировки -->
     <label for="sortSelect">Сортировать по:</label>
     <select id="sortSelect" v-model="store.sortOrder" @change="onSortOrderChange">
       <option value="asc">По возрастанию</option>
       <option value="desc">По убыванию</option>
     </select>
 
-    <ul>
-      <li v-for="comment in comments" :key="comment.id" @click="goToComment(comment.id)">
+    <!-- Список комментариев -->
+    <ul class="comments__list">
+      <li class="comments__list-item" v-for="comment in comments" :key="comment.id" @click="goToComment(comment.id)">
         <p>{{ comment.name }}</p>
       </li>
     </ul>
 
-    <button @click="prevPage" :disabled="store.currentPage === 1">Предыдущая</button>
-    <button @click="nextPage" :disabled="comments.length < 10">Следующая</button>
+    <!-- Навигация по станицам-->
+    <Pagination :currentPage="store.currentPage" :hasMoreComments="comments.length >= 10" :comments="comments"
+                @prev-page="prevPage" @next-page="nextPage" />
   </div>
 </template>
 
 <script>
 import store from '@/store/index.js';
-
+import Pagination from '../components/Pagination.vue';
 export default {
   name: 'IndexPage',
+  components: {
+    Pagination,
+  },
   computed: {
     store() {
       return store
@@ -41,10 +48,12 @@ export default {
   },
 
   methods: {
+    // Переход к деталям комментария по его ID
     goToComment(commentId) {
       this.$router.push(`/comments/${commentId}`);
     },
 
+    // Загрузка комментариев с сервера
     async fetchComments() {
       try {
         this.comments = await this.$axios.$get(`comments?_sort=id&_order=${this.store.sortOrder}&_page=${this.store.currentPage}&_limit=${this.perPage}`);
@@ -54,6 +63,7 @@ export default {
       }
     },
 
+    // Переход на предыдущую страницу комментариев
     async prevPage() {
       if (this.store.currentPage > 1) {
         this.store.currentPage--;
@@ -61,11 +71,13 @@ export default {
       }
     },
 
+    // Переход на следующую страницу комментариев
     async nextPage() {
       this.store.currentPage++;
       await this.fetchComments();
     },
 
+    // Обработка изменения порядка сортировки
     onSortOrderChange() {
       this.store.currentPage = 1; // сбросить текущую страницу на 1 при изменении фильтра
       this.fetchComments();
@@ -73,3 +85,7 @@ export default {
   },
 };
 </script>
+
+<style>
+
+</style>
